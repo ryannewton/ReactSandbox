6c9fe2fd01e3b35e9972b314233a3d4fe4731666
Ryan Newton <newton1988@gmail.com>
2017-01-17 15:50:27 -0500

    Adds thumbnail and title to VideoListItem component

--- VideoPlayerSite/src/components/video_list_item.js
+++ VideoPlayerSite/src/components/video_list_item.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 const VideoListItem = ({ video }) => {
+	const imageUrl = video.snippet.thumbnails.default.url;
+
 	return (
 		<li className="list-group-item">
+			<div className="video-list media">
+				<div classname="media-left">
+					<img className="media-object" src={imageUrl} />
+				</div>
+
+				<div className="media-body">
+					<div classname="media-heading">{video.snippet.title}</div>
+				</div>
+			</div>
 		</li>
 	);
 };
