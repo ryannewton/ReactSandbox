5016a2d17b4037fa6f5bd8a2bc33e7758e61f3ac
Ryan Newton <newton1988@gmail.com>
2017-01-17 13:55:56 -0500

    Gives SearchBar state and adds the user's search input to the state

--- VideoPlayerSite/src/components/search_bar.js
+++ VideoPlayerSite/src/components/search_bar.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+	}
+
 	render() {
-		return <input onChange={this.onInputChange} />;
+		return (
+			<div>
+				<input onChange={(this.onInputChange.bind(this))} />
+			</div>
+		);
 	}
 
 	// Good naming convention is 'on' or 'handle'
 	//  + name of element being watched
 	//  + name of event
 	onInputChange(event) {
-
+		this.setState({ term: event.target.value });
 	}
 };
 
