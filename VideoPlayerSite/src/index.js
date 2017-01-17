973e28d0c245b98b681305b58f5d121f46cdbdf9
Ryan Newton <newton1988@gmail.com>
2017-01-17 15:54:29 -0500

    Adds Youtube API call to index

--- VideoPlayerSite/src/index.js
+++ VideoPlayerSite/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 import API_KEY from '../api_keys';
 
 class App extends Component {
@@ -9,6 +10,10 @@ class App extends Component {
 		super(props);
 
 		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
 	}
 
 	render() {
