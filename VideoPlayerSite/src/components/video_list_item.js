b8b94e24597311f83fc854a928ba583386968cb5
Ryan Newton <newton1988@gmail.com>
2017-01-17 18:06:36 -0500

    Fixes styling typo classname -> className

--- VideoPlayerSite/src/components/video_list_item.js
+++ VideoPlayerSite/src/components/video_list_item.js
@@ -6,12 +6,12 @@ const VideoListItem = ({ video, onVideoSelect }) => {
 	return (
 		<li onClick={() => onVideoSelect(video)} className="list-group-item">
 			<div className="video-list media">
-				<div classname="media-left">
+				<div className="media-left">
 					<img className="media-object" src={imageUrl} />
 				</div>
 
 				<div className="media-body">
-					<div classname="media-heading">{video.snippet.title}</div>
+					<div className="media-heading">{video.snippet.title}</div>
 				</div>
 			</div>
 		</li>
