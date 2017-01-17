d7b50b4624d33f9768cde517acde99c0dca7dd17
Ryan Newton <newton1988@gmail.com>
2017-01-17 15:03:14 -0500

    Adds boilerplate for YouTube search API to index

--- VideoPlayerSite/src/index.js
+++ VideoPlayerSite/src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import API_KEY from '../api_keys';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
